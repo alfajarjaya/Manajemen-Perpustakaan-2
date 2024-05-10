3317f581ac1ae9dd3da744e456dbd57180420986
alfajarjaya <sansanjaya40@gmail.com>
2024-05-10 12:42:28 +0700

    new fitur

--- static/js/module/lb_client.js
+++ static/js/module/lb_client.js
@@ -2,195 +2,193 @@ const script = document.createElement("script");
 script.src = "/static/modules/sweetalert.js";
 document.head.appendChild(script);
 
+const emailjsScript = document.createElement("script");
+emailjsScript.src = "https://cdn.emailjs.com/dist/email.min.js";
+document.head.appendChild(emailjsScript);
+
 script.onload = () => {
-    const pinjamButtonsDesktop = document.querySelectorAll(".LB-Desktop .btn");
-    const pinjamButtonsMobile = document.querySelectorAll(".LB-Mobile .btn");
-    const myAlert = document.querySelector(".overlay");
+    emailjsScript.onload = () => {
+        emailjs.init("-7k1PYuT2Y3Tzfi0d");
 
-    const namaBukuInp = document.getElementById("nama_buku_input");
-    const namaBukuTag = document.getElementById("nama_buku");
+        const pinjamButtonsDesktop = document.querySelectorAll(".LB-Desktop .btn");
+        const pinjamButtonsMobile = document.querySelectorAll(".LB-Mobile .btn");
+        const myAlert = document.querySelector(".overlay");
 
-    const bookIdInp = document.getElementById("book_id");
+        const namaBukuInp = document.getElementById("nama_buku_input");
+        const namaBukuTag = document.getElementById("nama_buku");
 
-    const sisaBukuAtt = document.getElementById("sisa");
+        const bookIdInp = document.getElementById("book_id");
 
-    const penerbitBukuTagHtml = document.getElementById("penerbit");
+        const sisaBukuAtt = document.getElementById("sisa");
 
-    let globalSisa;
+        const penerbitBukuTagHtml = document.getElementById("penerbit");
 
-    pinjamButtonsDesktop.forEach((button, index) => {
-        button.addEventListener("click", () => {
-            myAlert.style.display = "block";
-            const bookId = button.getAttribute("data-book-id");
-            const bookName = button.getAttribute("data-book-name");
-            const authorName = button.getAttribute("data-book-author");
-            const sisa = button
-                .closest(".list")
-                .querySelector('[name="sisa_buku_input"]').value;
+        let globalSisa;
 
-            bookIdInp.value = bookId;
-            namaBukuInp.value = bookName;
-            namaBukuTag.innerText = "Judul : " + bookName;
-            penerbitBukuTagHtml.innerText = "Penerbit : " + authorName;
-            sisaBukuAtt.innerText = "Sisa : " + sisa;
+        pinjamButtonsDesktop.forEach((button, index) => {
+            button.addEventListener("click", () => {
+                myAlert.style.display = "block";
+                const bookId = button.getAttribute("data-book-id");
+                const bookName = button.getAttribute("data-book-name");
+                const authorName = button.getAttribute("data-book-author");
+                const sisa = button
+                    .closest(".list")
+                    .querySelector('[name="sisa_buku_input"]').value;
 
-            globalSisa = sisa;
+                bookIdInp.value = bookId;
+                namaBukuInp.value = bookName;
+                namaBukuTag.innerText = "Judul : " + bookName;
+                penerbitBukuTagHtml.innerText = "Penerbit : " + authorName;
+                sisaBukuAtt.innerText = "Sisa : " + sisa;
+
+                globalSisa = sisa;
+            });
         });
-    });
 
-    pinjamButtonsMobile.forEach((button, index) => {
-        button.addEventListener("click", () => {
-            myAlert.style.display = "block";
-            const bookId = button.getAttribute("data-book-id");
-            const bookName = button.getAttribute("data-book-name");
-            const authorName = button.getAttribute("data-book-author");
-            const sisa = button
-                .closest(".list")
-                .querySelector('[name="sisa_buku_input"]').value;
+        pinjamButtonsMobile.forEach((button, index) => {
+            button.addEventListener("click", () => {
+                myAlert.style.display = "block";
+                const bookId = button.getAttribute("data-book-id");
+                const bookName = button.getAttribute("data-book-name");
+                const authorName = button.getAttribute("data-book-author");
+                const sisa = button
+                    .closest(".list")
+                    .querySelector('[name="sisa_buku_input"]').value;
 
-            bookIdInp.value = bookId;
-            namaBukuInp.value = bookName;
-            namaBukuTag.innerText = "Judul : " + bookName;
-            penerbitBukuTagHtml.innerText = "Penerbit : " + authorName;
-            sisaBukuAtt.innerText = "Sisa : " + sisa;
+                bookIdInp.value = bookId;
+                namaBukuInp.value = bookName;
+                namaBukuTag.innerText = "Judul : " + bookName;
+                penerbitBukuTagHtml.innerText = "Penerbit : " + authorName;
+                sisaBukuAtt.innerText = "Sisa : " + sisa;
 
-            globalSisa = sisa;
+                globalSisa = sisa;
+            });
         });
-    });
 
-    const cancelBtn = document.querySelector("#btn-cancel");
-    cancelBtn.addEventListener("click", () => {
-        myAlert.style.display = "none";
-    });
+        const cancelBtn = document.querySelector("#btn-cancel");
+        cancelBtn.addEventListener("click", () => {
+            myAlert.style.display = "none";
+        });
 
-    const btnPinjam = document.getElementById("btn-pinjam");
-    const namaClient = document.getElementById("nama");
-    const kelasClient = document.getElementById("kelas");
-    const nisnClient = document.getElementById("NISN");
-    const tglPinjam = document.getElementById("pinjam");
+        const btnPinjam = document.getElementById("btn-pinjam");
+        const namaClient = document.getElementById("nama");
+        const kelasClient = document.getElementById("kelas");
+        const nisnClient = document.getElementById("NISN");
+        const tglPinjam = document.getElementById("pinjam");
 
-    btnPinjam.addEventListener("click", () => {
-        const bookId = bookIdInp.value;
-        const bookName = namaBukuInp.value;
-        const nama = namaClient.value;
-        const kelas = kelasClient.value;
-        const nisn = nisnClient.value;
-        const tglValue = tglPinjam.value;
-        const newCount = globalSisa;
-        sendToServer(bookId, bookName, newCount, nama, kelas, nisn, tglValue);
-    });
-};
+        btnPinjam.addEventListener("click", () => {
+            const bookId = bookIdInp.value;
+            const bookName = namaBukuInp.value;
+            const nama = namaClient.value;
+            const kelas = kelasClient.value;
+            const nisn = nisnClient.value;
+            const tglValue = tglPinjam.value;
+            const newCount = globalSisa;
 
-function sendToServer(
-    bookId,
-    bookName,
-    newCount,
-    nama,
-    kelas,
-    nisn,
-    tglPinjam
-) {
-    if (newCount !== null) {
-        const xhr = new XMLHttpRequest();
-        xhr.open("POST", "/pinjamBuku", true);
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.onreadystatechange = () => {
-            if (xhr.readyState === XMLHttpRequest.DONE) {
-                const response = JSON.parse(xhr.responseText);
-                if (xhr.status === 200) {
-                    Swal.fire({
-                        title: "Berhasil",
-                        text: response.message,
-                        icon: "success",
-                        showCancelButton: false,
-                        confirmButtonColor: "#3085d6",
-                        cancelButtonColor: "#d33",
-                        confirmButtonText: "Oke",
-                    }).then((result) => {
-                        if (result.isConfirmed) {
+            sendToServer(bookId, bookName, newCount, nama, kelas, nisn, tglValue);
+        });
+
+        function sendToServer(
+            bookId,
+            bookName,
+            newCount,
+            nama,
+            kelas,
+            nisn,
+            tglPinjam
+        ) {
+            if (newCount !== null) {
+                const xhr = new XMLHttpRequest();
+                xhr.open("POST", "/pinjamBuku", true);
+                xhr.setRequestHeader("Content-Type", "application/json");
+                xhr.onreadystatechange = () => {
+                    if (xhr.readyState === XMLHttpRequest.DONE) {
+                        const response = JSON.parse(xhr.responseText);
+                        if (xhr.status === 200) {
+                            sendToEmailJS(bookId, bookName, newCount, nama, kelas, nisn, tglPinjam);
                             Swal.fire({
-                                text: "Mohon Tunggu Halaman sedang di reload.",
-                                icon: "warning",
-                                showConfirmButton: false,
+                                title: "Berhasil",
+                                text: response.message,
+                                icon: "success",
+                                showCancelButton: false,
+                                confirmButtonColor: "#3085d6",
+                                cancelButtonColor: "#d33",
+                                confirmButtonText: "Oke",
+                            }).then((result) => {
+                                if (result.isConfirmed) {
+                                    Swal.fire({
+                                        text: "Mohon Tunggu Halaman sedang di reload.",
+                                        icon: "warning",
+                                        showConfirmButton: false,
+                                    });
+                                    window.location.reload();
+                                } else {
+                                    const Toats = Swal.mixin({
+                                        showConfirmButton: false,
+                                        timerProgressBar: true,
+                                        timer: 2000,
+                                    });
+                                    Toats.fire({
+                                        icon: "warning",
+                                        title: "Mohon tunggu halaman sedang di reload.",
+                                    });
+                                    setTimeout(() => {
+                                        window.location.reload();
+                                    }, 2000);
+                                }
                             });
-                            window.location.reload();
                         } else {
-                            const Toats = Swal.mixin({
-                                showConfirmButton: false,
-                                timerProgressBar: true,
-                                timer: 2000,
+                            Swal.fire({
+                                title: "Gagal",
+                                text: response.message,
+                                icon: "error",
+                                showCancelButton: false,
+                                confirmButtonColor: "#3085d6",
+                                cancelButtonColor: "#d33",
+                                confirmButtonText: "Oke",
                             });
-                            Toats.fire({
-                                icon: "warning",
-                                title: "Mohon tunggu halaman sedang di reload.",
-                            });
-                            setTimeout(() => {
-                                window.location.reload();
-                            }, 2000);
                         }
-                    });
-                } else {
-                    Swal.fire({
-                        title: "Gagal",
-                        text: response.message,
-                        icon: "error",
-                        showCancelButton: false,
-                        confirmButtonColor: "#3085d6",
-                        cancelButtonColor: "#d33",
-                        confirmButtonText: "Oke",
-                    });
-                }
+                    }
+                };
+                const data = JSON.stringify({
+                    bookId: bookId,
+                    bookName: bookName,
+                    newCount: newCount,
+                    nama: nama,
+                    kelas: kelas,
+                    nisn: nisn,
+                    tglPinjam: tglPinjam,
+                });
+                xhr.send(data);
+                tglPinjam.value = "";
             }
-        };
-        const data = JSON.stringify({
-            bookId: bookId,
-            bookName: bookName,
-            newCount: newCount,
-            nama: nama,
-            kelas: kelas,
-            nisn: nisn,
-            tglPinjam: tglPinjam,
-        });
-        xhr.send(data);
-        tglPinjam.value = "";
-    }
-}
-
-function sendToWhatsaap(
-    bookId,
-    bookName,
-    newCount,
-    nama,
-    kelas,
-    nisn,
-    tglPinjam
-) {
-    const nodemailer = require("nodemailer");
-
-    // Buat transporter untuk mengirim email
-    let transporter = nodemailer.createTransport({
-        service: "Gmail",
-        auth: {
-            user: "your-email@gmail.com",
-            pass: "your-password",
-        },
-    });
-
-    // Buat objek email
-    let mailOptions = {
-        from: `${nama}`,
-        to: "alfajjar123@gmail.com",
-        subject: "Subject of your email",
-        text: "Here is the body text of your email.",
-    };
-
-    // Kirim email
-    transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log("Email sent: " + info.response);
         }
-    });
-    window.open(link);
-}
+
+        function sendToEmailJS(
+            bookId,
+            bookName,
+            newCount,
+            nama,
+            kelas,
+            nisn,
+            tglPinjam
+        ) {
+            const newNewCount = newCount - 1;
+            const tglKembali = new Date(tglPinjam);
+            tglKembali.setDate(tglKembali.getDate() + 7);
+            const tglKembaliFormatted = tglKembali.toISOString().split("T")[0];
+
+            emailjs.send("alfajjar", "alfajjar", {
+                bookId: bookId,
+                bookName: bookName,
+                newCount: newCount,
+                newNewCount: newNewCount,
+                nama: nama,
+                kelas: kelas,
+                nisn: nisn,
+                tglPinjam: tglPinjam,
+                tglKembali: tglKembaliFormatted,
+            });
+        }
+    };
+};
